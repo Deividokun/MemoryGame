f8d4b13227a555efbdf51d2e65aa40a8688c4690
Deividokun <129647532+Deividokun@users.noreply.github.com>
2024-04-17 11:36:53 +0200

    Commit inicial

---
+++ main.js
@@ -0,0 +1,233 @@
+import './style.css'
+const MainAndGame = () => {
+  return `
+  <h1>Memory UFC</h1>
+  <button>Play Again</button>
+  <div class="grid-container">
+  <div class="card" >
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSrDryzGWlgPXBR9vQ8rvysJcBXRT9wrMLJfrgrHiImEOcVn337" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card" >
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+      <img src="https://fotografias.lasexta.com/clipping/cmsimages01/2020/04/04/7E839305-7051-433E-AEDF-9469CD2C45AD/104.jpg?crop=800,800,x287,y0&width=1200&height=1200&optimize=low&format=webply" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-10/MAKHACHEV_ISLAM_BELT_L_10-21.png?itok=glu3eHJa" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-10/MAKHACHEV_ISLAM_BELT_L_10-21.png?itok=glu3eHJa" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://s3.superluchas.com/2022/04/St-Pierre-GSP-1.jpg" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+      <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSrDryzGWlgPXBR9vQ8rvysJcBXRT9wrMLJfrgrHiImEOcVn337" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://fotografias.lasexta.com/clipping/cmsimages01/2020/04/04/7E839305-7051-433E-AEDF-9469CD2C45AD/104.jpg?crop=800,800,x287,y0&width=1200&height=1200&optimize=low&format=webply" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://image.ondacero.es/clipping/cmsimages02/2024/02/15/1EAEC493-DC23-4B6F-AE43-9D3C64D77152/ilia-topuria-foto-archivo_104.jpg?crop=683,683,x181,y0&width=1200&height=1200&optimize=low&format=webply" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://ih1.redbubble.net/image.4433669833.8910/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://hips.hearstapps.com/hmg-prod/images/khabib-nurmagomedov-of-russia-poses-on-the-scale-during-the-news-photo-1608027423.?crop=0.66689xw:1xh;center,top&resize=2048:*" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://s3.superluchas.com/2022/04/St-Pierre-GSP-1.jpg" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-03/JONES_JON_L_BELT_03_04.png?itok=P6J6DQpm" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://hips.hearstapps.com/hmg-prod/images/khabib-nurmagomedov-of-russia-poses-on-the-scale-during-the-news-photo-1608027423.?crop=0.66689xw:1xh;center,top&resize=2048:*" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://ih1.redbubble.net/image.4433669833.8910/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+        <img src="https://dmxg5wxfqgb4u.cloudfront.net/styles/athlete_bio_full_body/s3/2023-03/JONES_JON_L_BELT_03_04.png?itok=P6J6DQpm" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/UFC_Logo.svg/2560px-UFC_Logo.svg.png" alt="">
+      </div>
+      <div class="card-back">
+      <img src="https://image.ondacero.es/clipping/cmsimages02/2024/02/15/1EAEC493-DC23-4B6F-AE43-9D3C64D77152/ilia-topuria-foto-archivo_104.jpg?crop=683,683,x181,y0&width=1200&height=1200&optimize=low&format=webply" alt="">
+    </div>
+  </div>
+  </div>
+    
+  `
+}
+
+const LogicGame = () => {
+  let arrayShuffle = []
+  console.log(arrayShuffle)
+  //cartas
+  let firstCard = null
+  let firstCardElement = null
+  //contador
+  let counterN = 0
+  //celdas
+  const cellClass = document.querySelectorAll('.card')
+  console.log(arrayShuffle)
+  ///Pop up creado
+  const popup = document.createElement('div')
+  popup.classList.add('popup')
+  popup.textContent = 'Has ganado en el memory game de la UFC!'
+  ///array recorrido
+  cellClass.forEach((card) => {
+    card.addEventListener('click', (ev) => {
+      const backImage = card.querySelector('.card-back img')
+      const srcImageBack = backImage.getAttribute('src')
+      console.log(arrayShuffle)
+      if (firstCard === null) {
+        firstCard = srcImageBack
+        firstCardElement = card
+        card.classList.add('is-flipped')
+      } else if (firstCard === srcImageBack) {
+        card.classList.add('is-flipped')
+        counterN++
+        firstCard = null
+      } else if (firstCard !== srcImageBack) {
+        card.classList.add('is-flipped')
+        setTimeout(() => {
+          card.classList.remove('is-flipped')
+          firstCardElement.classList.remove('is-flipped')
+          firstCard = null
+          firstCardElement = null
+        }, 1000)
+      }
+      if (counterN === 8) {
+        document.body.appendChild(popup)
+        setTimeout(() => {
+          popup.classList.add('POPUP')
+        }, 2000)
+      }
+    })
+  })
+}
+const playAgain = () => {
+  const button = document.querySelector('button')
+  const cellClass = document.querySelectorAll('.card')
+  cellClass.forEach((card) => {
+    button.addEventListener('click', () => {
+      card.classList.remove('is-flipped')
+    })
+  })
+}
+const PrintTemplateGame = () => {
+  const main = document.querySelector('main')
+  main.innerHTML = MainAndGame()
+  LogicGame()
+  playAgain()
+}
+document.addEventListener('DOMContentLoaded', PrintTemplateGame)
